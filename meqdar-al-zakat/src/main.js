0d98ba9f3a881f74fe95ea2414f7580b14b35dba
Eyad Nasr <eyadnasr8@gmail.com>
2025-03-24 22:31:28 +0400

    Added gold and silver market prices API

--- meqdar-al-zakat/src/main.js
+++ meqdar-al-zakat/src/main.js
@@ -1,2 +1,17 @@
-// import "./style.css";
-console.log("Hello World!");
+import $ from "jquery";
+
+$("#input-nisab").on("change", () => {
+  let nisabValue = 0;
+  if ($("#input-nisab").val() == "gold") {
+    $.get("https://api.gold-api.com/price/XAU", function (data) {
+      nisabValue = data;
+      console.log(nisabValue);
+      $("#nisab-value").text(nisabValue.price);
+    });
+  } else if ($("#input-nisab").val() == "silver") {
+    $.get("https://api.gold-api.com/price/XAG", function (data) {
+      nisabValue = data;
+      $("#nisab-value").text(nisabValue.price);
+    });
+  }
+});
