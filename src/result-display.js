6dacc0986ce2a5e5de679704b6aacdfee8627244
Eyad Nasr <eyadnasr8@gmail.com>
2025-04-16 19:26:23 +0400

    Updated background design and default result box values

--- src/result-display.js
+++ src/result-display.js
@@ -16,18 +16,25 @@ function fillMoneyResultBox(
 function fillLivestockResultBox(camelZakat, cattleZakat, sheepZakat) {
   function processLivestockZakat(resultPlaceholder, zakat) {
     resultPlaceholder.empty();
+    let count = 0;
     let listItem;
     for (const [key, value] of Object.entries(zakat)) {
       if (!(value === 0 || value === "")) {
         if (key.includes("type")) {
-          listItem = $(`<li>${value}</li>`);
+          listItem.append(` ${value}`);
         } else if (key.includes("count")) {
-          listItem.append(`: ${value}`);
+          listItem = $(`<li>${value}</li>`);
           resultPlaceholder.append(listItem);
         } else if (key.includes("operation")) {
-          resultPlaceholder.append($(`<li>${key}: ${value}</li>`));
+          resultPlaceholder.append($(`<li>${value}</li>`));
         }
+        count++;
       }
+    }
+
+    if (count === 0) {
+      // No items returned => No zakat to be paid
+      resultPlaceholder.append($("<li>0</li>"));
     }
   }
 
@@ -37,10 +44,10 @@ function fillLivestockResultBox(camelZakat, cattleZakat, sheepZakat) {
 }
 
 function fillCropsResultBox(wheatZakat, barleyZakat, datesZakat, raisinsZakat) {
-  $("#result-wheat").text(wheatZakat);
-  $("#result-barley").text(barleyZakat);
-  $("#result-dates").text(datesZakat);
-  $("#result-raisins").text(raisinsZakat);
+  $("#result-wheat").text(wheatZakat.count + " " + wheatZakat.unit);
+  $("#result-barley").text(barleyZakat.count + " " + barleyZakat.unit);
+  $("#result-dates").text(datesZakat.count + " " + datesZakat.unit);
+  $("#result-raisins").text(raisinsZakat.count + " " + raisinsZakat.unit);
 }
 
 function fillGoodsResultBox(
